0e9970967d186b40dab4384563a90dd6fa4bfd5a
Lauren Eggers <neyabenz@gmail.com>
2019-05-22 19:31:24 -0400

    style changes

--- js/toDo.js
+++ js/toDo.js
@@ -1,21 +1,23 @@
+
+//cross off items
+$('ol').on('click', 'li', function() {
+    $(this).toggleClass('done');
+})
+
+//delete items
+$('ol').on('click', 'span', function(e) {
+    $(this).parent().fadeOut(800, function() {
+        $(this).remove();
+    })
+    e.stopPropagation();
+})
 //input to add new li
-$('input').on('keypress', function(e){
+$('input[type="text"]').on('keypress', function(e){
     if(e.which === 13) {
         $(this).attr('disabled', 'disabled')
         let text = $.parseHTML($(this).val())
         $('ol').append('<li><span>X</span> ' + text[0].textContent + '</li>');
         $(this).removeAttr('disabled')
+        $(this).val('')
     }
-})
-//cross off items
-$('li').on('click', function() {
-    $(this).toggleClass('done');
-})
-
-//delete items
-$('span').on('click', function(e) {
-    $(this).parent().fadeOut(800, function() {
-        $(this).remove();
-    })
-    e.stopPropagation();
 })
